4be89b84d347f50621144c9cdd567a7c67d3962f
Im jungje <ijj@cryptofuture.co.kr>
2019-03-19 13:00:21 +0900

    header scatter 연동

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,39 +1,34 @@
 <template>
 	<header style="padding:0 9.1%;position:relative;z-index:2;">
 		<nav id="nav">
-			<router-link to="/baccarat" tag="div" style="font-size: 18px;
-  font-weight: normal;
-  font-style: normal;
-  font-stretch: normal;
-  
-  letter-spacing: normal;">
-	<!-- Baccarat -->
-	</router-link>
-			<router-link to="#bonus" tag="div" style="font-size: 18px;
+			<router-link to="/" tag="div" style="-webkit-flex: 4;flex: 4;text-align: left;">
+				<img src="@/assets/logo.png"/>
+			</router-link>
+			<div style="display:flex;justify-content: space-between;-webkit-flex: 5;flex: 5;align-items: baseline;">
+	<router-link to="#bonus" tag="div" style="font-size: 18px;
   font-weight: normal;
   font-style: normal;
   font-stretch: normal;
   letter-spacing: normal;">
-	<!-- Bonus -->
+	Baccarat Lobby
 	</router-link>
 			<router-link to="#vip" tag="div" style="font-size: 18px;
   font-weight: normal;
   font-style: normal;
   font-stretch: normal;
   letter-spacing: normal;">
-	<!-- VIP -->
+	Bonus
 	</router-link>
 			<router-link to="#referral" tag="div" style="font-size: 18px;
   font-weight: normal;
   font-style: normal;
   font-stretch: normal;
   letter-spacing: normal;">
-	<!-- Referral -->
+	Free CPU
 	</router-link>
-			<router-link to="/" tag="div"><img src="@/assets/logo.png"/></router-link>
-			<div style="line-height:0;"><img src="@/assets/speaker.png"/></div>
+			<!-- <div style="line-height:0;"><img src="@/assets/speaker.png"/></div> -->
 			<div>
-				<!-- EOS -->
+				Fairness
 				</div>
 			<div style="width: 199px;
 		height: 30px;
@@ -45,15 +40,18 @@
 				<span v-if="!eosAccount" @click="proc_getIdentity">Log in with <span style="font-weight: bold;">Scatter</span></span>
 				<span v-else @click="proc_forgetIdentity"><span>{{eosAccount.name}}</span></span>
 			</div>
+			</div>
 		</nav>
 	</header>
 </template>
 
 <script>
-import ScatterJS from 'scatterjs-core'
-import ScatterEOS from 'scatterjs-plugin-eosjs'
+import { mapGetters, mapActions } from 'vuex'
+// import ScatterJS from 'scatterjs-core'
+// import ScatterEOS from 'scatterjs-plugin-eosjs'
+// ScatterJS.plugins( new ScatterEOS() );
 import Eos from 'eosjs';
-ScatterJS.plugins( new ScatterEOS() );
+
 
 export default {
 		name: 'appheader',
@@ -62,12 +60,13 @@ export default {
 			return {
 				tosvr:{
 					set_scatter_identity (scatter_identity = undefined ) {
-						//if (!ws || !game_connected) return;
-						const scatter = this.scatter
+						console.log("set_scatter_identity")
+						if (!that.$socket || !that.game_connected) return;
+						const scatter = that.scatter
 
 						if (scatter_identity === undefined) {          
 							if ( scatter && scatter.identity ) {
-								const eosAccount = this.scatter.identity.accounts.find(account => account.blockchain === 'eos');
+								const eosAccount = that.scatter.identity.accounts.find(account => account.blockchain === 'eos');
 								scatter_identity = eosAccount.name; 
 								console.log('스캐터 아이디', scatter_identity);
 							}
@@ -80,28 +79,39 @@ export default {
 							identity  : scatter_identity
 						};
 
+						console.log("req_json",req_json)
 						that.$socket.send(JSON.stringify(req_json));
-					}
+					},
 				},
-				eosAccount: null,
-				scatter: ScatterJS.scatter,
+				//eosAccount: null,
+				//scatter: ScatterJS.scatter,
 				eos: null,
-				network: ScatterJS.Network.fromJson({
-					name: 'Kylin',
-					blockchain:'eos',
-					chainId:'5fff1dae8dc8e2fc4d5b23b2c7665c97f9e9d8edf2b6485a86ba311c25639191',
-					host:'api-kylin.eoslaomao.com',
-					port:443,
-					protocol:'https'
-				}),
+				
 			}
 		},
+		computed: {
+			...mapGetters({
+				eosAccount: 'getEosAccount',
+				scatter: 'getScatter',
+				network: 'getNetwork',
+				game_connected: 'getGameConnected',
+			}),
+		},
+		mounted() {
+			this.connectScatter()
+    	//this.$connect()
+		},
 		methods: {
+			...mapActions({
+				setEosAccount: 'setEosAccount',
+				setCredit: 'setCredit',
+			}),
 			async proc_forgetIdentity() {
 				if (!this.scatter.identity) return; 
 
 				await this.scatter.forgetIdentity();
-				this.eosAccount = null;
+				this.setEosAccount(null)
+				this.setCredit(0)
 				
 				this.tosvr.set_scatter_identity('');
 			},
@@ -118,8 +128,9 @@ export default {
 				await this.scatter.getIdentity({accounts:[this.network]})
 				.then(identity => {
 					console.log("getIdentity: 성공");
-					let eosAccount = this.eosAccount = identity.accounts.find(account => account.blockchain === 'eos');
-					console.log(this.eosAccount);               
+					this.setEosAccount(identity.accounts.find(account => account.blockchain === 'eos'));
+					let eosAccount = this.eosAccount
+					console.log("eosAccount",eosAccount);
 
 					// 게임서버에 identity 알림. 
 					this.tosvr.set_scatter_identity(eosAccount.name);
@@ -132,32 +143,25 @@ export default {
 		
 				//console.log(this.scatter.identity);
 			},
-			connectScatter(){
-				return new Promise((resolve, reject)=>{
-					const connectionOptions = {initTimeout:3000};
-					ScatterJS.scatter.connect('game-eosbaccarat3', connectionOptions).then( connected => {
-						if(!connected){          
-							console.log('Could not connect to Scatter.');
-							alert('Please download Scatter if it is not installed')
-							return;
-						}        
-						console.log('Scatter Connected!')
-						this.eos = this.scatter.eos(this.network, Eos);        
+			async connectScatter(){
+				const connectionOptions = {initTimeout:5000};
+				const connected = await this.scatter.connect('game-eosbaccarat3', connectionOptions)
+									
+				if(!connected){          
+					console.log('Could not connect to Scatter.');
+					//alert('Please download Scatter if it is not installed')
+					return;
+				}
 
-						if (this.scatter.identity) {
-							this.eosAccount = this.scatter.identity.accounts.find(account => account.blockchain === 'eos');
-						}else{
-							this.eosAccount = null;
-						}
-
-						resolve(this.tosvr.set_scatter_identity(eosAccount.name));
-					}).catch( ()=>{
-						resolve(this.tosvr.set_scatter_identity(''));
-					} )
-
-				})
-					
-				
+				console.log('Scatter Connected!')
+				this.eos = this.scatter.eos(this.network, Eos);          
+				if (this.scatter.identity) {            
+					this.setEosAccount(this.scatter.identity.accounts.find(account => account.blockchain === 'eos'));
+					this.tosvr.set_scatter_identity(this.eosAccount.name);
+				}else{
+					this.setEosAccount(null)
+					this.tosvr.set_scatter_identity('');
+				}      
 			},
 		}
 }
@@ -171,7 +175,7 @@ export default {
 	background-color: #000000;
   display: flex;
 	justify-content: space-between;
-	padding: 48px 4.2% 44px;
+	padding: 48px 0 44px;
 	align-items: flex-end;
 	div {
 		cursor: pointer;
