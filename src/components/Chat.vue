2709def5eb19012a51678a4a5e7a3cc603365785
Im jungje <ijj@cryptofuture.co.kr>
2019-03-22 15:03:12 +0900

    chat 소스 정리

--- src/components/Chat.vue
+++ src/components/Chat.vue
@@ -1,173 +1,198 @@
 <template>
-	<div>
-        <div :style="{
-					width: '67px',
-					height: '213px',
-					'box-shadow': '-3.5px 2px 10px 0 rgba(0, 0, 0, 0.46)',
-					'background-color': '#ff343d',
-					'border-radius': '15px 0 0 15px',
-					'font-weight': '600',
-    			'font-style': 'normal',
-    			'font-stretch': 'normal',
-    'line-height': 'normal',
-    'letter-spacing': 'normal',
-    'text-align': 'center',
-    'color': '#ffffff',
-    'position': 'absolute',
-    'top': '651px',
-    'right': show?'12%':'0',
-    'z-index': '3',
-    'cursor': 'pointer',
-    'font-size':'20px',
-      }"
-      @click="clickShow">
-      <img src="@/assets/user.png" style="margin-top: 30px;"/>
-      <span style="writing-mode: tb-rl;transform: rotate(180deg);
+  <div id="chat_wrap">
+    <div class="chat_btn" :style="{'right': show?'12%':'0'}" @click="clickShow">
+      <img src="@/assets/user.png">
+      <span>Live chat</span>
+    </div>
+    <transition nmae="slide-fade">
+      <div class="chat_box_wrap">
+        <div v-if="show" class="chatbox">
+          <div class="chat-header">
+            <i class="icon-bet-chat"></i>
+            <div class="users-online"></div>
+            <a class="close">
+              <span></span>
+            </a>
+          </div>
+          <div
+            class="chats"
+            id="chat-messages-container"
+            @scroll="checkScroll"
+            @mouseenter="scrollCheck"
+            @mouseleave="scrollCheck"
+          >
+            <!---->
+            <div class="chat" v-for="(data, i) in messageList" :key="i">
+              <span class="username">{{data.name}}</span>
+              <span class="time">{{data.time}}</span>
+              <p>{{data.msg}}</p>
+            </div>
+          </div>
+          <div class="input">
+            <!---->
+            <!-- <div class="btn-cont">
+							<a class="btn btn-primary btn-block">Login</a>
+            </div>-->
+            <input
+              v-model="message"
+              v-if="eosAccount!=null"
+              name="message"
+              type="text"
+              placeholder="Write message"
+              maxlength="200"
+              autocomplete="off"
+              @keydown.enter="msgSend"
+            >
+          </div>
+        </div>
+      </div>
+    </transition>
+  </div>
+</template>
+<script>
+import { mapGetters } from "vuex";
+
+export default {
+  name: "appChat",
+  data() {
+    return {
+      show: false,
+      message: "",
+      messageList: [],
+      scrollPosition: 0
+    };
+  },
+  computed: {
+    ...mapGetters({
+      eosAccount: "getEosAccount"
+    })
+  },
+  mounted() {
+    this.$chat.onmessage = mes => {
+      const message = JSON.parse(mes.data);
+      message.forEach(data => {
+        data.time = this.$moment(data.time).format("HH:mm");
+      });
+      this.messageList = message;
+      if (this.show) {
+        this.$nextTick(() => {
+          var container = this.$el.querySelector("#chat-messages-container");
+          //console.log("container.scrollHeight",container.scrollHeight)
+          container.scrollTop = container.scrollHeight;
+        });
+      }
+    };
+  },
+  watch: {
+    show: function() {
+      if (this.show) {
+        this.$nextTick(() => {
+          var container = this.$el.querySelector("#chat-messages-container");
+          //console.log("container.scrollHeight",container.scrollHeight)
+          container.scrollTop = container.scrollHeight;
+        });
+      }
+    }
+  },
+  methods: {
+    msgSend() {
+      this.$chat.send(
+        JSON.stringify({
+          type: "chat",
+          user: this.eosAccount.name,
+          txt: this.message
+        })
+      );
+      this.message = "";
+    },
+    clickShow() {
+      this.show = !this.show;
+      this.$parent.show = this.show;
+    },
+    checkScroll(e) {
+      // e.stopPropagation();
+      // console.log("eee",e)
+      // const top = Math.round(e.srcElement.scrollTop)
+      // const height = e.srcElement.scrollHeight-e.srcElement.clientHeight
+      // if(e.srcElement.scrollTop < 1){
+      // 	e.srcElement.scrollTop = 1
+      // }else if(e.srcElement.scrollHeight-e.srcElement.scrollTop-e.srcElement.clientHeight < 1){
+      // 	e.srcElement.scrollTop = e.srcElement.scrollTop - 1
+      // }
+      // console.log(Math.round(e.srcElement.scrollTop),)
+      // // if(top>=height){
+      // // }
+    },
+    scrollCheck(e) {
+      //console.log("scrollCheck",e)
+      if (e.type == "mouseleave") {
+        document.body.style.overflowY = "auto";
+      } else if (e.type == "mouseenter") {
+        document.body.style.overflowY = "hidden";
+      }
+    }
+  }
+};
+</script>
+<style lang="scss" scoped>
+// .w20 {
+//   -webkit-transform: translateX(0);
+//   transform: translateX(0);
+//   width: 20%;
+//   max-height: 100%;
+//   position: fixed;
+//   top: 0;
+//   right: 0;
+//   z-index: 2000;
+// }
+// .tfw {
+//   transition: width 0.25s ease-out, transform 0.25s ease-out, padding-top 0.5s,
+//     -webkit-transform 0.25s ease-out;
+// }
+#chat_wrap {
+  .chat_btn {
+    width: 67px;
+    height: 213px;
+    box-shadow: -3.5px 2px 10px 0 rgba(0, 0, 0, 0.46);
+    background-color: #ff343d;
+    border-radius: 15px 0 0 15px;
+    font-weight: 600;
+    font-style: normal;
+    font-stretch: normal;
+    line-height: normal;
+    letter-spacing: normal;
+    text-align: center;
+    color: #ffffff;
+    position: absolute;
+    top: 651px;
+    z-index: 3;
+    cursor: pointer;
+    font-size: 20px;
+    img {
+      margin-top: 30px;
+    }
+    span {
+      writing-mode: tb-rl;
+      transform: rotate(180deg);
       position: absolute;
       top: 92px;
       right: 21px;
-      ">Live chat</span>
-      </div>
-      <transition nmae="slide-fade">
-				<div style="position: fixed;
-    top: 0;
-    right: 0;
-    width: 12%;
-		z-index: 3;
-		">
-        <div v-if="show" class="chatbox">
-					<div class="chat-header"><i class="icon-bet-chat"></i><div class="users-online"></div><a class="close"><span></span></a></div>
-					<div class="chats" id="chat-messages-container"
-					@scroll="checkScroll"
-					@mouseenter="scrollCheck"
-					@mouseleave="scrollCheck"
-					>
-						<!---->
-						<div class="chat" v-for="(data, i) in messageList" :key="i">
-							<span class="username">{{data.name}}</span>
-							<span class="time">{{data.time}}</span>
-							<p>{{data.msg}}</p>
-						</div>
-					</div>
-					<div class="input">
-						<!---->
-						<!-- <div class="btn-cont">
-							<a class="btn btn-primary btn-block">Login</a>
-						</div> -->
-						<input v-model="message" v-if="eosAccount!=null" name="message" type="text" placeholder="Write message" maxlength="200" autocomplete="off"
-						@keydown.enter="msgSend">
-					</div>
-        </div>
-				</div>
-      </transition>
-    </div>
-</template>
-<script>
-import { mapGetters } from 'vuex'
-
-export default {
-	name: 'appChat',
-	data() {
-		return {
-			show: false,
-			message: '',
-			messageList: [],
-			scrollPosition: 0,
-		}
-	},
-	computed: {
-		...mapGetters({
-			eosAccount: 'getEosAccount',
-		}),
-	},
-	mounted(){
-
-		this.$chat.onmessage = (mes)=> {
-			const message = JSON.parse(mes.data)
-			message.forEach(data => {
-				data.time = this.$moment(data.time).format('HH:mm')
-			})
-			this.messageList = message
-			if(this.show){
-				this.$nextTick(() => {
-					var container = this.$el.querySelector("#chat-messages-container");
-					//console.log("container.scrollHeight",container.scrollHeight)
-					container.scrollTop = container.scrollHeight;
-				});
-			}
-		}
-	},
-	watch: {
-		show: function(){
-			if(this.show){
-				this.$nextTick(() => {
-					var container = this.$el.querySelector("#chat-messages-container");
-					//console.log("container.scrollHeight",container.scrollHeight)
-					container.scrollTop = container.scrollHeight;
-				});
-			}
-		}
-	},
-	methods: {
-		msgSend(){
-			this.$chat.send(JSON.stringify({
-				type: 'chat',
-				user: this.eosAccount.name,
-				txt:this.message
-			}))
-			this.message = ''
-		},
-		clickShow(){
-			this.show = !this.show
-			this.$parent.show = this.show
-		},
-		checkScroll(e){
-			// e.stopPropagation();
-			// console.log("eee",e)
-
-			// const top = Math.round(e.srcElement.scrollTop)
-			// const height = e.srcElement.scrollHeight-e.srcElement.clientHeight
-			// if(e.srcElement.scrollTop < 1){
-			// 	e.srcElement.scrollTop = 1
-			// }else if(e.srcElement.scrollHeight-e.srcElement.scrollTop-e.srcElement.clientHeight < 1){
-			// 	e.srcElement.scrollTop = e.srcElement.scrollTop - 1
-			// }
-			// console.log(Math.round(e.srcElement.scrollTop),)
-			// // if(top>=height){
-			// // }
-		},
-		scrollCheck(e){
-			//console.log("scrollCheck",e)
-			if(e.type=="mouseleave"){
-				document.body.style.overflowY = 'auto'
-			}else if(e.type=="mouseenter"){
-				document.body.style.overflowY = 'hidden'
-			}
-		}
-	}
-}
-</script>
-<style lang="scss" scoped>
-.w20 {
-    -webkit-transform: translateX(0);
-    transform: translateX(0);
-    width: 20%;
-    max-height: 100%;
+    }
+  }
+  .chat_box_wrap {
     position: fixed;
     top: 0;
     right: 0;
-    z-index: 2000;
-}
-.tfw {
-    transition: width .25s ease-out,transform .25s ease-out,padding-top .5s,-webkit-transform .25s ease-out;
-}
-.chat-header {
-    width: 100%;
-    display: block;
-    position: absolute;
-    top: 0;
-    height: 60px;
-    i.icon-bet-chat {
+    width: 12%;
+    z-index: 3;
+
+    .chat-header {
+      width: 100%;
+      display: block;
+      position: absolute;
+      top: 0;
+      height: 60px;
+      i.icon-bet-chat {
         display: block;
         position: absolute;
         top: 0;
@@ -177,101 +202,103 @@ export default {
         height: 60px;
         line-height: 60px;
         text-align: center;
-        font-size: 24px!important;
+        font-size: 24px !important;
+      }
+      .icon-bet-chat:before {
+        content: "\e905";
+      }
+      .users-online {
+        position: absolute;
+        left: 60px;
+        display: block;
+        width: 120px;
+        height: 60px;
+        top: 0;
+        line-height: 60px;
+      }
+      .close {
+        float: right;
+        font-size: 1.3125rem;
+        font-weight: 700;
+        line-height: 1;
+        color: #000;
+        text-shadow: 0 1px 0 #fff;
+        opacity: 0.5;
+      }
+      .close:not(:disabled):not(.disabled) {
+        cursor: pointer;
+      }
     }
-    .icon-bet-chat:before {
-      content: "\e905";
+    .chatbox {
+      height: 100vh;
+      z-index: 1337;
+      background: #141b28;
+      color: #fff;
+      padding-top: 60px;
+      padding-bottom: 50px;
+      box-sizing: border-box;
+      .chats {
+        overflow-y: scroll;
+        height: 100%;
+      }
+      .chat {
+        display: block;
+        float: left;
+        clear: both;
+        width: 100%;
+        box-sizing: border-box;
+        padding: 15px;
+        .username {
+          float: left;
+          height: 16px;
+          display: block;
+          font-weight: 700;
+          font-size: 14px;
+          line-height: 16px;
+          color: #878;
+          min-width: 170px;
+          text-align: left;
+        }
+        .time {
+          float: right;
+          height: 16px;
+          display: block;
+          color: #6685a5;
+          font-size: 12px;
+          line-height: 16px;
+        }
+        p {
+          float: left;
+          display: block;
+          clear: both;
+          overflow-wrap: break-word;
+          max-width: 100%;
+          font-size: 12px;
+          margin: 4px 0 0;
+          line-height: calc(23 / 14);
+          text-align: left;
+        }
+      }
+      .input {
+        background: #1d2235;
+        height: 50px;
+        width: 100%;
+        z-index: 100;
+        position: fixed;
+        bottom: 0;
+        input {
+          height: 30px;
+          float: left;
+          padding-left: 15px;
+          margin-top: 10px;
+          width: 85%;
+          background: 0 0;
+          border: none;
+          color: #fff;
+        }
+      }
     }
-    .users-online {
-      position: absolute;
-      left: 60px;
-      display: block;
-      width: 120px;
-      height: 60px;
-      top: 0;
-      line-height: 60px;
-  	}
-		.close {
-			float: right;
-			font-size: 1.3125rem;
-			font-weight: 700;
-			line-height: 1;
-			color: #000;
-			text-shadow: 0 1px 0 #fff;
-			opacity: .5;
-		}
-		.close:not(:disabled):not(.disabled) {
-			cursor: pointer;
-		}
-}
-.chatbox {
-	height: 100vh;
-	z-index: 1337;
-	background: #141b28;
-	color: #fff;
-	padding-top: 60px;
-	padding-bottom: 50px;
-	box-sizing: border-box;
-	.chats {
-			overflow-y: scroll;
-			height: 100%;
-	}
-	.chat {
-		display: block;
-		float: left;
-		clear: both;
-		width: 100%;
-		box-sizing: border-box;
-		padding: 15px;
-		.username {
-				float: left;
-				height: 16px;
-				display: block;
-				font-weight: 700;
-				font-size: 14px;
-				line-height: 16px;
-				color: #878;
-				min-width: 170px;
-				text-align: left;
-		}
-		.time {
-				float: right;
-				height: 16px;
-				display: block;
-				color: #6685a5;
-				font-size: 12px;
-				line-height: 16px;
-		}
-		p {
-				float: left;
-				display: block;
-				clear: both;
-				overflow-wrap: break-word;
-				max-width: 100%;
-				font-size: 12px;
-				margin: 4px 0 0;
-				line-height: calc(23/14);
-				text-align: left;
-		}
-	}
-	.input {
-		background: #1d2235;
-		height: 50px;
-		width: 100%;
-		z-index: 100;
-		position: fixed;
-		bottom: 0;
-		input {
-			height: 30px;
-			float: left;
-			padding-left: 15px;
-			margin-top: 10px;
-			width: 85%;
-			background: 0 0;
-			border: none;
-			color: #fff;
-		}
-	}
+  }
 }
 </style>
 
